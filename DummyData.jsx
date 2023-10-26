fc6e502ca6caa6ded4e58ee5e7c1e1ad47eb37ed
Areeba Shakeel <shakeelareeba02@gmail.com>
2023-10-26 13:11:47 +0530

    Create DummyData.jsx

---
+++ DummyData.jsx
@@ -0,0 +1,66 @@
+const Data = [
+    {
+        id:1,
+        name: "Jaipur",
+        description: "Spain, a country",
+        image: "https://images.thrillophilia.com/image/upload/s--wae0uYSz--/c_fill,f_auto,fl_strip_profile,h_600,q_auto,w_975/v1/images/photos/000/133/219/original/1538040658_shutterstock_1150544138.jpg.jpg?1538040658",
+        price: '$790',
+    },
+    {
+        id:1,
+        name: "Agra",
+        description: "Mauritius, an Indian Ocean",
+        image: "https://tse4.mm.bing.net/th?id=OIP.lBOlADau6JpMJT0gIYwEVwHaE8&pid=Api&P=0&h=180",
+        price: '$590',
+    },
+    {
+        id:1,
+        name: "Landour",
+        description: "Spain, a country",
+        image: "https://i1.wp.com/travelshoebum.com/wp-content/uploads/2019/07/IMG_20190518_093955.jpg?resize=1000%2C750&ssl=1",
+        price: '$790',
+    },
+    {
+        id:1,
+        name: "Landour",
+        description: "Spain, a country",
+        image: "https://i1.wp.com/travelshoebum.com/wp-content/uploads/2019/07/IMG_20190518_093955.jpg?resize=1000%2C750&ssl=1",
+        price: '$790',
+    },
+    {
+        id:1,
+        name: "Jaipur",
+        description: "Spain, a country",
+        image: "https://images.thrillophilia.com/image/upload/s--wae0uYSz--/c_fill,f_auto,fl_strip_profile,h_600,q_auto,w_975/v1/images/photos/000/133/219/original/1538040658_shutterstock_1150544138.jpg.jpg?1538040658",
+        price: '$790',
+    },
+    {
+        id:1,
+        name: "Agra",
+        description: "Mauritius, an Indian Ocean",
+        image: "https://tse4.mm.bing.net/th?id=OIP.lBOlADau6JpMJT0gIYwEVwHaE8&pid=Api&P=0&h=180",
+        price: '$590',
+    },
+    {
+        id:1,
+        name: "Landour",
+        description: "Spain, a country",
+        image: "https://i1.wp.com/travelshoebum.com/wp-content/uploads/2019/07/IMG_20190518_093955.jpg?resize=1000%2C750&ssl=1",
+        price: '$790',
+    },
+    {
+        id:1,
+        name: "Landour",
+        description: "Spain, a country",
+        image: "https://i1.wp.com/travelshoebum.com/wp-content/uploads/2019/07/IMG_20190518_093955.jpg?resize=1000%2C750&ssl=1",
+        price: '$790',
+    },
+    {
+        id:1,
+        name: "Landour",
+        description: "Spain, a country",
+        image: "https://i1.wp.com/travelshoebum.com/wp-content/uploads/2019/07/IMG_20190518_093955.jpg?resize=1000%2C750&ssl=1",
+        price: '$790',
+    },
+]
+export default Data;
