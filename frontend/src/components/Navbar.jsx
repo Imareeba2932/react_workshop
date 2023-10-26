5db1f035b31174e5eb61fa07b9f52b833c04fd1f
Areeba Shakeel <shakeelareeba02>
2023-10-26 13:38:28 +0530

    banner

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-            <nav className="navbar bg-danger border-bottom border-body navbar-expand-lg" >
-                <div className="container-fluid">
+            <nav className="navbar bg-danger border-bottom border-body navbar-expand-lg " >
+                <div className="container-fluid ">
                     {/* <a className="navbar-brand" href="#">Navbar</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button> */}
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
+                    <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNavAltMarkup">
+                        <div className="navbar-nav ">
                             <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
                         </div>
                     </div>
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import sea from './sea.mp4'
 const Signup = () => {
   return (
-    <div className='container-fluid mt-5 mb-5 vid' >
+    <div className='container-fluid mt-5 vid' >
         <div className="signup-content d-flex mx-auto flex-column align-items-center w-100" style={{zIndex: 2}}>
           <h4 className='text-light fw-bold mt-5'>Sign Up for 35% Discount</h4>
           <h5 className='text-light'>Want to get an instant discount for your next</h5>
